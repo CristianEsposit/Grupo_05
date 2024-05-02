ccb9225fd9d27154994b8aaa072673b2340d49cb
Cristian <esposito.cristian.g@gmail.com>
2024-05-01 22:36:29 -0300

    cambios en main

---
+++ SubiQueTeLlevo/src/modelo/Cliente.java
@@ -0,0 +1,31 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Cliente extends Usuario {
+	private ArrayList<Viaje> viajes = new ArrayList<Viaje>();
+
+	public Cliente(String nombreReal, String nombreUsuario, String password) {
+		super(nombreReal, nombreUsuario, password);
+	}
+
+	public void agregaViaje(Viaje viaje) {
+		this.viajes.add(viaje);
+	}
+
+	public ArrayList<Viaje> getViajes() {
+		return viajes;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente " + super.toString() + "[viajes=" + viajes + "]";
+	}
+	
+	public Object clone() throws CloneNotSupportedException{
+		Cliente nCli = (Cliente)super.clone();
+		nCli.viajes = ( ArrayList<Viaje> )this.viajes.clone();
+		return nCli;
+	}
+
+}
